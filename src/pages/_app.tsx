3c908c8caa9bfdc995ec04bb46dbc9d8df5ea6e2
JOAO PEDROZO <jp.pedrozo@hotmail.com>
2023-03-20 12:00:30 -0300

    chore: setup chakra-ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+
+      {/* We need to do this to make sure that the page have at least 100% height */}
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          height: 100%;
+        }
+      `}</style>
+    </ChakraProvider>
+  );
 }
