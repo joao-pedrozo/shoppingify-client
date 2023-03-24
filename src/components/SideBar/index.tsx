48b4f2010ea57c7dc8580768e25fd2454405a105
Ciro Lacerda <cirolacerda@gmail.com>
2023-03-24 12:48:34 -0300

    Feature shopping list #8 (#11)
    * chore: add react-icons
    
    * feat: create card button
    
    * feat: add shopping list component
    
    ---------
    
    Co-authored-by: Bruno Pinheiro <brunocroh@gmail.com>

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,10 +1,19 @@
 import { Flex } from '@chakra-ui/react';
 import { NavigationItems } from '../NavigationItems';
+import { ShoppingList } from '../ShoppingList';
 
 export const SideBar = () => {
   return (
-    <Flex bg="white" width="24" h="full" alignItems={'center'}>
+    <Flex
+      bg="white"
+      width="24"
+      h="full"
+      justify="space-between"
+      alignItems="center"
+      direction="column"
+    >
       <NavigationItems />
+      <ShoppingList />
     </Flex>
   );
 };
