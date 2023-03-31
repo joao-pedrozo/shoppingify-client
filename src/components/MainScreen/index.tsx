d8dd952dd6d67572de7fafdb5c7712488a4a7f4f
Denise Recofka <recofka@gmail.com>
2023-03-31 11:25:14 +0200

    Update src/components/MainScreen/index.tsx
    Co-authored-by: Bruno Pinheiro <brunocroh@gmail.com>

--- src/components/MainScreen/index.tsx
+++ src/components/MainScreen/index.tsx
@@ -3,8 +3,7 @@ import { SideBar } from '@/components/SideBar';
 import { SideDrawer } from '@/components/SideDrawer';
 import { ProductCategory } from '@/components/ProductCategory';
 
-export const MainScreen = () => {
-  const listOfCategoriesWithItems = [{
+const mockListOfCategoriesWithItems = [{
     title: 'Fruit and Vegetables',
     items: ['Avocado ', 'Banana', 'Bunch of purple carrots 5pcs', 'Watermelon', 'Mandarin Nadorcott', 'Orange', 'Spinach', 'Cucumber', 'Broccoli']
   }, {
@@ -14,16 +13,16 @@ export const MainScreen = () => {
   {
     title: 'Bakery',
     items: ['Bread', 'Whole Grain Bread', 'Chocolate Cake', 'Butter Cake', 'Croissant', 'Pan au Chocolat']
-  }
-  ];
+}];
 
+export const MainScreen = () => {
   return (
     <Flex bg="#fafafe" w="full" justifyContent="space-between" >
       <SideBar />
       <Box w='100%' h='100vh' overflowY='scroll'>
-        <ProductCategory {...listOfCategoriesWithItems[0]}></ProductCategory>
-        <ProductCategory {...listOfCategoriesWithItems[1]}></ProductCategory>
-        <ProductCategory {...listOfCategoriesWithItems[2]}></ProductCategory>
+        <ProductCategory {...mockListOfCategoriesWithItems[0]}></ProductCategory>
+        <ProductCategory {...mockListOfCategoriesWithItems[1]}></ProductCategory>
+        <ProductCategory {...mockListOfCategoriesWithItems[2]}></ProductCategory>
       </Box>
       <SideDrawer />
     </Flex>
