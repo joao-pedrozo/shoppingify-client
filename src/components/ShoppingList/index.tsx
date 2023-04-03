7091ac322b79f5ca062c5dd137b83ee483d4a2d3
Denis Silva <denisls02@outlook.com>
2023-04-02 23:20:12 -0300

    message

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,17 +1,33 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, Badge, Text } from '@chakra-ui/react';
 import { MdOutlineShoppingCart } from 'react-icons/md';
-
+import { useState } from 'react';
 export const ShoppingList = () => {
+  //Counting will be recieved from another component
+  const [count] = useState(4);
   return (
-    <Flex h="42px" w="42">
+    <Flex>
       <Button
-        h="full"
-        w="full"
         backgroundColor="#F9A109"
         borderRadius="50%"
+        w="35"
+        h="45"
         color={'white'}
       >
         <MdOutlineShoppingCart fontSize={20} />
+        <Badge
+          pos={'absolute'}
+          left="35px"
+          top="-5px"
+          variant="solid"
+          bg="red"
+          borderRadius="4px"
+          px={'5px'}
+          py="1px"
+        >
+          <Text color="#FFFFFF" fontSize="16">
+            {count}
+          </Text>
+        </Badge>
       </Button>
     </Flex>
   );
