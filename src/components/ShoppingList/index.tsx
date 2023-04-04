479035afc73732af1d228d51c37e7960c627b652
Luizefz <luizefsilva2@gmail.com>
2023-04-04 17:22:03 -0300

    fix: shoppingList - Buttom and badge adjust

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,17 +1,39 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Flex, Button, Text, Badge } from '@chakra-ui/react';
+import { useState } from 'react';
 import { MdOutlineShoppingCart } from 'react-icons/md';
 
 export const ShoppingList = () => {
+  const [count] = useState(3);
+
   return (
-    <Flex h="42px" w="42">
+    <Flex w="full" justify="center">
       <Button
-        h="full"
-        w="full"
-        backgroundColor="#F9A109"
+        position="relative"
+        w="46px"
+        h="46px"
         borderRadius="50%"
-        color={'white'}
+        color="white"
+        bg="#F9A109"
+        _hover={{ bg: '#f5bb4b' }}
+        _active={{
+          bg: '#f5bb4b',
+        }}
       >
-        <MdOutlineShoppingCart fontSize={20} />
+        <Text fontSize={24}>
+          <MdOutlineShoppingCart />
+        </Text>
+
+        <Badge
+          position="absolute"
+          left="34px"
+          top="-6px"
+          p="2px 6px"
+          borderRadius="4px"
+          variant="solid"
+          bg="#EB5757"
+        >
+          <Text fontSize={12}>{count}</Text>
+        </Badge>
       </Button>
     </Flex>
   );
