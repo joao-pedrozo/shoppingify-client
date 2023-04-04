d47fcdeb58f4bd6a4b640e19523d69d6fe945885
Renata Karolina <renatakarolinarko@gmail.com>
2023-04-03 22:15:38 -0400

    added searchbar component

--- src/components/MainScreen/index.tsx
+++ src/components/MainScreen/index.tsx
@@ -1,10 +1,12 @@
 import { Flex } from '@chakra-ui/react';
 import { SideBar } from '@/components/SideBar';
 import { SideDrawer } from '@/components/SideDrawer';
+import { SearchBar } from '../SearchBar';
 
 export const MainScreen = () => (
   <Flex bg="#f1f1f1" w="full" justifyContent="space-between">
     <SideBar />
+    <SearchBar />
     <SideDrawer />
   </Flex>
 );
