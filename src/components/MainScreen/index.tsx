485957a146830aaf80e61dcb949a69cd6bd5134d
Denise Recofka <recofka@gmail.com>
2023-04-06 16:45:50 +0200

    Product category (#17)
    * feat:ProductCategory and ProductItem component creation
    
    * feat: Adding Chakra theme with font
    
    * feat: Adjust overflow to scroll and margins
    
    * Update src/components/ProductCategory/index.tsx
    
    Co-authored-by: Bruno Pinheiro <brunocroh@gmail.com>
    
    * Update src/components/MainScreen/index.tsx
    
    Co-authored-by: Bruno Pinheiro <brunocroh@gmail.com>
    
    * Update src/components/ProductCategory/index.tsx
    
    Co-authored-by: Bruno Pinheiro <brunocroh@gmail.com>
    
    * feat: Code review changes
    
    ---------
    
    Co-authored-by: Denise Recofka <denise@Robinsons-MBP.home>
    Co-authored-by: Bruno Pinheiro <brunocroh@gmail.com>

--- src/components/MainScreen/index.tsx
+++ src/components/MainScreen/index.tsx
@@ -1,10 +1,21 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { SideBar } from '@/components/SideBar';
 import { SideDrawer } from '@/components/SideDrawer';
+import { ProductCategory } from '@/components/ProductCategory';
+import { mockListOfCategoriesWithItems } from '@/components/ProductCategory/__mock__/productCategoryData';
 
-export const MainScreen = () => (
-  <Flex bg="#f1f1f1" w="full" justifyContent="space-between">
-    <SideBar />
-    <SideDrawer />
-  </Flex>
-);
+export const MainScreen = () => {
+  return (
+    <Flex bg="#fafafe" w="full" justifyContent="space-between" >
+      <SideBar />
+      <Box w='100%' h='100vh' overflowY='scroll'>
+        <ProductCategory {...mockListOfCategoriesWithItems[0]}></ProductCategory>
+        <ProductCategory {...mockListOfCategoriesWithItems[1]}></ProductCategory>
+        <ProductCategory {...mockListOfCategoriesWithItems[2]}></ProductCategory>
+      </Box>
+      <SideDrawer />
+    </Flex>
+  )
+};
+
+
---
+++ src/components/ProductCategory/__mock__/productCategoryData.ts
@@ -0,0 +1,36 @@
+import { Category } from '../index';
+
+export const mockListOfCategoriesWithItems: Category[] = [
+  {
+    id: 1,
+    title: 'Fruit and Vegetables',
+    items: [
+      'Avocado',
+      'Banana',
+      'Bunch of purple carrots 5pcs',
+      'Watermelon',
+      'Mandarin Nadorcott',
+      'Orange',
+      'Spinach',
+      'Cucumber',
+      'Broccoli',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Beverages',
+    items: ['Soda ', 'Cola', 'Orange Juice', 'Beer Pilsener 6x', 'Wine'],
+  },
+  {
+    id: 3,
+    title: 'Bakery',
+    items: [
+      'Bread',
+      'Whole Grain Bread',
+      'Chocolate Cake',
+      'Butter Cake',
+      'Croissant',
+      'Pan au Chocolat',
+    ],
+  },
+];
