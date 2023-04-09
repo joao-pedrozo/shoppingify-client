1900154658bd9d9a2dc69ac83c5b3f23edd5a51e
Luiz Eduardo <67990430+Luizefz@users.noreply.github.com>
2023-04-09 13:34:49 -0300

    fix: shoppingList - Buttom and badge adjust (#21)
    * fix: shoppingList - Buttom and badge adjust
    
    * refactor: changed the shoppinglist button to chakra iconbutton

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,40 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Flex, IconButton, Text, Badge } from '@chakra-ui/react';
+import { useState } from 'react';
 import { MdOutlineShoppingCart } from 'react-icons/md';
 
 export const ShoppingList = () => {
+  const [count] = useState(3);
+
   return (
-    <Flex h="42px" w="42">
-      <Button
-        h="full"
-        w="full"
-        backgroundColor="#F9A109"
+    <Flex w="full" justify="center">
+      <IconButton
+        position="relative"
+        w="42px"
+        h="42px"
         borderRadius="50%"
-        color={'white'}
-      >
-        <MdOutlineShoppingCart fontSize={20} />
-      </Button>
+        color="white"
+        bg="#F9A109"
+        _hover={{ bg: '#FF8E22' }}
+        _active={{ bg: '#D5771D' }}
+        aria-label={'Shopping cart'}
+        icon={
+          <>
+            <MdOutlineShoppingCart fontSize={22} />
+
+            <Badge
+              position="absolute"
+              left="34px"
+              top="-5px"
+              p="2px 6px"
+              borderRadius="4px"
+              variant="solid"
+              bg="#EB5757"
+            >
+              <Text fontSize={12}>{count}</Text>
+            </Badge>
+          </>
+        }
+      />
     </Flex>
   );
 };
